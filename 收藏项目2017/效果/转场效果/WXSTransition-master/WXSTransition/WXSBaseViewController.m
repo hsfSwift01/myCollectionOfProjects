b9df4419f519b3df1074d6dcf61e974e236da4b9
gitConfig01 <hsf_swift_01@sina.com>
2017-03-03 16:01:46 +0800

    添加转场效果 demo
    添加转场效果 demo

---
+++ 收藏项目2017/效果/转场效果/WXSTransition-master/WXSTransition/WXSBaseViewController.m
@@ -0,0 +1,32 @@
+//
+//  WXSBaseViewController.m
+//  WXSTransition
+//
+//  Created by thejoyrun on 16/7/8.
+//  Copyright © 2016年 王小树. All rights reserved.
+//
+
+#import "WXSBaseViewController.h"
+
+@interface WXSBaseViewController ()
+
+@end
+
+@implementation WXSBaseViewController
+
+-(void)dealloc {
+    NSLog(@"%@ dealloc",  NSStringFromClass([self class]));
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+
+@end
